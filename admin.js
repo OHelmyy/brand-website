ef58f783e84208a1fcbd0c00a96b384cbf12987e
Amr Ahmed <sobhyamr521@gmail.com>
2024-05-07 02:19:32 +0300

    update js
    adding js for add user

--- admin.js
+++ admin.js
@@ -76,3 +76,84 @@ addWindow.style.display = 'none';
   
   
   //
+
+
+  function addInput() {
+    var firstName = document.getElementById('firstName').value;
+    var lastName = document.getElementById('lastName').value;
+    var username = document.getElementById('username').value;
+    var email = document.getElementById('email').value;
+    var address = document.getElementById('address').value;
+    var phone = document.getElementById('phone').value;
+    var password = document.getElementById('password').value;
+
+    var firstNameError = document.getElementById('firstNameError');
+    var lastNameError = document.getElementById('lastNameError');
+    var emailError = document.getElementById('emailError');
+    var passwordError = document.getElementById('passwordError');
+    var usernameError = document.getElementById('usernameError');
+    var phoneError = document.getElementById('phoneError');
+    var addressError = document.getElementById('addressError');
+
+    var err = true;
+
+    if(firstName == "") {
+        firstNameError.innerHTML = " first name is Required";
+        err = false;
+    } else if(!/^[a-zA-Z\s]+$/.test(firstName)) {
+        firstNameError.innerHTML = "Please enter a valid first name";
+        err = false;
+    } else {
+        firstNameError.innerHTML = "";
+    }
+
+    if(lastName == "") {
+        lastNameError.innerHTML = "last name is required";
+        err = false;
+    } else if(!/^[a-zA-Z\s]+$/.test(lastName)) {
+        lastNameError.innerHTML = "Please enter a valid last name";
+        err = false;
+    } else {
+        lastNameError.innerHTML = "";
+    }
+
+    if(email == "") {
+        emailError.innerHTML = " email is required";
+        err = false;
+    } else if(!/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(email)) {
+        emailError.innerHTML = "Please enter a valid email";
+        err = false;
+    } else {
+        emailError.innerHTML = "";
+    }
+
+    if(password == "" ){
+        passwordError.innerHTML = "Password Is Required";
+        err = false;
+    } else {
+        passwordError.innerHTML = "";
+    }
+
+    if(username == "" ){
+        usernameError.innerHTML = "username Is Required";
+        err = false;
+    } else {
+        usernameError.innerHTML = "";
+    }
+
+    if(phone == ""){
+        phoneError.innerHTML = " phone number Is Required";
+        err = false;
+    } else {
+        phoneError.innerHTML = "";
+    }
+
+    if(address == ""){
+        addressError.innerHTML = " address is required";
+        err = false;
+    } else {
+        addressError.innerHTML = "";
+    }
+
+    return err;
+}
